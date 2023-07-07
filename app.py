45ce899cef8d01578124ea973afabceb4de7d824
keyafm <123993768+keyafm@users.noreply.github.com>
2023-07-07 20:42:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import git
 
 app = Flask(__name__)
 
 # Cargamos el modelo
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
-
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./pythonanywhere')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
